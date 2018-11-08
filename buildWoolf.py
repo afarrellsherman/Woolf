c951f7383a621776d7d7c0f35fe4ddc29eeec57b
afarrel4 <afarrel4@wellesley.edu>
2018-11-08 12:28:33 -0500

    adding buildWoolf and updating timeline

---
+++ buildWoolf.py
@@ -0,0 +1,17 @@
+# ############################### #
+# buildWoolf.py
+# 
+# Outline of script to build a woolf model.
+# 
+# Anna Farrell-Sherman 11/5/18
+# ############################### #
+
+
+def main():
+
+
+
+
+
+
+main()
