92e4368355b1beafed81d326b370932744317651
afarrel4 <afarrel4@wellesley.edu>
2019-06-12 11:12:01 -0400

    added testing fasta files

---
+++ tests/test_woolf_unit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+####################################
+#
+# unitTests.py
+# Testing code for the Woolf Pipeline
+#
+####################################
+
+import pytest
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
