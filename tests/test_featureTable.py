b1652dfc89095fd56f185f04e846bf70ee6eb56c
Kevin Bonham <kevbonham@gmail.com>
2019-06-26 16:11:24 -0400

    use temporary directory for output files

--- tests/test_featureTable.py
+++ tests/test_featureTable.py
@@ -10,9 +10,12 @@
 import pytest
 from woolf import featureTable
 import os.path
+import tempfile
 
 testdata = os.path.join(os.path.dirname(__file__), 'data')
-outputdata = os.path.join(os.path.dirname(__file__), 'output')
+tmp = tempfile.TemporaryDirectory()
+outputdata = tmp.name
+
 testclasses = [os.path.join(testdata, f) for f in os.listdir(testdata)]
 testclasses.sort()
 classA, classB, classC, classD = testclasses[1:]
@@ -44,16 +47,16 @@ def test_predictFeatureTable_CD():
 	assert list(pTable.columns) == ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'ID', 'K', 'L', 'Length', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 	assert len(pTable.index) == 41
 
-# def test_saveCSV_classC_predictTable():
-# 	pTable = featureTable.predictFeatureTable([classC])
-# 	outputfilename = os.path.join(outputdata, 'classC_predictTable')
-#
-# 	featureTable.saveCSV(pTable, outputfilename)
-# 	assert os.path.isfile(outputfilename)
-#
-# def test_saveCSV_classBD_binaryTable():
-# 	bTable = featureTable.binaryFeatureTable([classB], [classD])
-# 	outputfilename = os.path.join(outputdata, 'classBD_binaryTable')
-#
-# 	featureTable.saveCSV(bTable, outputfilename)
-# 	assert os.path.isfile(outputfilename)
+def test_saveCSV_classC_predictTable():
+	pTable = featureTable.predictFeatureTable([classC])
+	outputfilename = os.path.join(outputdata, 'classC_predictTable')
+
+	featureTable.saveCSV(pTable, outputfilename)
+	assert os.path.isfile(outputfilename)
+
+def test_saveCSV_classBD_binaryTable():
+	bTable = featureTable.binaryFeatureTable([classB], [classD])
+	outputfilename = os.path.join(outputdata, 'classBD_binaryTable')
+
+	featureTable.saveCSV(bTable, outputfilename)
+	assert os.path.isfile(outputfilename)
