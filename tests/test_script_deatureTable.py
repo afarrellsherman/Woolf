6788e1366681d61cde8e47afc43421de3dc98a67
Kevin Bonham <kevbonham@gmail.com>
2019-06-27 11:18:35 -0400

    move to single script-running test file

--- tests/test_script_deatureTable.py
+++
@@ -1,4 +0,0 @@
-def test_trainWoolf(script_runner):
-    ret = script_runner.run('featureTable', '--help')
-    assert ret.success
-    assert ret.stderr == ''
--- tests/test_script_trainWoolf.py
+++
@@ -1,5 +0,0 @@
-def test_trainWoolf(script_runner):
-    ret = script_runner.run('trainWoolf', '--help')
-    assert ret.success
-    assert ret.stderr == ''
-    
